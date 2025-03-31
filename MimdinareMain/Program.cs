25b40deb0d5e1a80720352d18a5aa0231550228a
XenoArt <165677372+XenoArt@users.noreply.github.com>
2025-03-30 17:36:40 -0800

    Update Program.cs

--- MimdinareMain/Program.cs
+++ MimdinareMain/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using MimdinareMain.Data;
 using MimdinareMain.Services;
-using MimdinareMain.Data;
-using MimdinareMain.Services;
 using MimdinareMain.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("mimdinare")));
@@ -20,7 +18,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure pipeline
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -35,7 +33,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    await db.Database.MigrateAsync();
+    await db.Database.MigrateAsync(); // This will apply any pending migrations to the database
 }
 
 await app.RunAsync();
